c6e02ae54b3c3dc8a2fd966cee048b3f5a8a52bf
Martin Büttner <m.buettner.d@gmail.com>
2014-06-21 12:51:25 +0100

    Add time limits and exception handling

--- controller.rb
+++ controller.rb
@@ -3,6 +3,8 @@
 # e.g.
 # ruby controller.rb benchmark.txt ruby randomracer.rb
 
+require 'timeout'
+
 # Patch a method into IO for more convenient nonblocking IO.
 # Found at http://stackoverflow.com/a/948077/1633117
 class IO
@@ -34,53 +36,83 @@ tracks.map do |input|
     turns = 0
     error = reached_goal = hit_wall = out_of_bounds = timed_out = false
 
-    # Give one second per turn
-    time_left = target
+    # Give half a second per turn
+    time_per_turn = 0.5
+    time_budget = time_per_turn * target
 
     racer = IO.popen(racer_command, 'r+')
 
     racer.puts input
-    racer.each do |line|
-        if !line[/^\s*[+-]?[01]\s+[+-]?[01]\s*$/]
-            puts "Invalid move: #{line}"
-            error = true
-            break
-        end
+    racer.puts time_budget
+    racer.flush
 
-        turns += 1
-        $stderr.puts "Racer says: #{line}"
+    last_time = Time.now
 
-        move = line.split.map(&:to_i)
+    begin
+        Timeout::timeout(2*time_budget) do
+            racer.each do |line|
+                current_time = Time.now
+                extra_time = current_time - last_time - time_per_turn
+                last_time = current_time
 
-        velocity = [position[0] - last_position[0],
-                    position[1] - last_position[1]]
+                time_budget -= extra_time if extra_time > 0
+                timed_out = time_budget <= 0
 
-        velocity[0] += move[0]
-        velocity[1] += move[1]
+                if !line[/^\s*[+-]?[01]\s+[+-]?[01]\s*$/]
+                    puts "Invalid move: #{line}"
+                    error = true
+                    break
+                end
 
-        last_position = position
-        position = [last_position[0] + velocity[0],
-                    last_position[1] + velocity[1]]
+                turns += 1
+                $stderr.puts "Racer says: #{line}"
 
-        if position[0] < 0 || position[0] >= size[0] ||
-           position[1] < 0 || position[1] >= size[1]
-            out_of_bounds = true
-        else
-            case track[position[1]][position[0]]
-            when '#'
-                hit_wall = true
-            when '*'
-                reached_goal = true
+                move = line.split.map(&:to_i)
+
+                velocity = [position[0] - last_position[0],
+                            position[1] - last_position[1]]
+
+                velocity[0] += move[0]
+                velocity[1] += move[1]
+
+                last_position = position
+                position = [last_position[0] + velocity[0],
+                            last_position[1] + velocity[1]]
+
+                if position[0] < 0 || position[0] >= size[0] ||
+                   position[1] < 0 || position[1] >= size[1]
+                    out_of_bounds = true
+                else
+                    case track[position[1]][position[0]]
+                    when '#'
+                        hit_wall = true
+                    when '*'
+                        reached_goal = true
+                    end
+                end
+
+                break if timed_out ||
+                         out_of_bounds ||
+                         hit_wall ||
+                         reached_goal ||
+                         racer.closed? ||
+                         turns >= target
+
+                racer.puts "Racer says: #{line}"
+                racer.flush
+
             end
         end
+    rescue Timeout::Error => e
+        timed_out = true
 
-        break if timed_out ||
-                 out_of_bounds ||
-                 hit_wall ||
-                 reached_goal ||
-                 turns >= target
-
-        racer.puts "Racer says: #{line}"
+        # Kill the process manually, otherwise we might have to
+        # wait for it to finish before closing.
+        Process.kill("KILL", racer.pid)
+    rescue Exception => e
+        puts e.message
+        puts e.backtrace.inspect
+        error = true
     end
 
     racer.close
@@ -88,19 +120,22 @@ tracks.map do |input|
     score = reached_goal ? turns/target.to_f : 2
     total_score += score
 
+    print "SCORE: %1.5f " % score
     if reached_goal
-        puts "SCORE: #{score}. Racer reached goal at #{position} in #{turns} turns."
+        puts "Racer reached goal at #{position} in #{turns} turns."
     elsif error
-        puts "SCORE: #{score}. Racer produced error."
+        puts "Racer produced error."
     elsif out_of_bounds
-        puts "SCORE: #{score}. Racer went out of bounds at position #{position}."
+        puts "Racer went out of bounds at position #{position}."
     elsif hit_wall
-        puts "SCORE: #{score}. Racer hit a wall at position #{position}."
+        puts "Racer hit a wall at position #{position}."
+    elsif timed_out
+        puts "Racer timed out after #{turns} turns."
     elsif turns >= target
-        puts "SCORE: #{score}. Racer did not reach the goal within #{target} turns."
+        puts "Racer did not reach the goal within #{target} turns."
     else
-        puts "SCORE: #{score}. Racer stopped before reaching goal."
+        puts "Racer stopped before reaching goal."
     end
 end
 
-puts "TOTAL SCORE: #{total_score}"
+puts "TOTAL SCORE: %1.5f" % total_score
--- randomracer.rb
+++ randomracer.rb
@@ -1,9 +1,19 @@
+# Parse initial input
 target = gets.to_i
 size = gets.split.map(&:to_i)
 track = []
 size[1].times do
     track.push gets
 end
+time_budget = gets.to_f
+
+# Find start position
+start_y = track.find_index { |row| row['S'] }
+start_x = track[start_y].index 'S'
+
+position = [start_x, start_y]
+velocity = [0, 0]
+
 x = rand(3) - 1
 y = rand(3) - 1
 puts [x,y].join ' '
